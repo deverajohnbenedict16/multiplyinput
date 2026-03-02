185af2462db3877610c1fb411a47f114a369ad09
deverajohnbenedict16 <deverajohnbenedict16@gmail.com>
2026-03-02 22:35:24 +0800

    Add files via upload

---
+++ multiplynum.py
@@ -0,0 +1,6 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+product = float(num1) * float(num2)
+
+print(f"The product of {num1} and {num2} is: {product}")
